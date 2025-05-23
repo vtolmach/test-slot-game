99929841ff6f6b479adf07817c1621841e68288f
Viktor <victor.web.developer@gmail.com>
2025-05-23 19:24:41 +0200

    cors hotifx

--- server/src/config/common.ts
+++ server/src/config/common.ts
@@ -13,7 +13,7 @@ const config: Config = {
     port: Number(process.env.PORT) || 3000,
     nodeEnv: process.env.NODE_ENV || 'development',
     sessionSecret: process.env.SESSION_SECRET || 'some-default-secret',
-    corsOrigin: [ process.env.CORS_ORIGIN || 'http://localhost:5173' ],
+    corsOrigin: [ process.env.CORS_ORIGIN || '/http[s]\/\/localhost:*/' ],
 };
 
 export default config;
